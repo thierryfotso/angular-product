7e22d11bdafe4825c75491431100f46fc8123e8d
Thierry FOTSO <fotsonono2003@yahoo.fr>
2025-05-26 18:29:36 +0200

    - introduction of class Product - introduction loop for with option

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,18 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product.model';
 
 @Component({
   selector: 'app-products',
-  standalone:true,
-  imports:[],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
 export class ProductsComponent implements OnInit {
-  products: string[];
+  products: Product[];
 
   constructor() {
-    this.products = ['PC Asus', 'Imprimante Epson', 'Tablette Samsung'];
+    this.products = [
+      {
+        productId:1,
+        productName:'PC Asus',
+        productPrice: 3000,
+        creationDate: new Date("01/14/2011")
+      },
+      {
+        productId:2,
+        productName:"Imprimante Epson",
+        productPrice:450,
+        creationDate: new Date("12/17/2010"),
+      },
+      {
+        productId:3,
+        productName:"Tablette Samsung",
+        productPrice:900.123,
+        creationDate: new Date("02/20/2020"),
+      }
+    ];
   }
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+
+  }
+
 }
