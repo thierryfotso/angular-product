8bfabea39e6ad814e848431145fc69939da9d98b
Thierry FOTSO <fotsonono2003@yahoo.fr>
2025-05-29 22:49:24 +0200

    - introduce environment.ts

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,10 +1,9 @@
-import { CAT_API_URL } from './../config';
 import { Injectable } from '@angular/core';
 import { Product } from '../model/product.model';
 import { Category } from '../model/category.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { API_URL } from '../config';
+import { environment } from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
@@ -19,29 +18,29 @@ export class ProductsService {
   constructor(private httpClient: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.httpClient.get<Product[]>(API_URL);
+    return this.httpClient.get<Product[]>(environment.API_URL);
   }
 
   getProduct(productId: number): Observable<Product> {
-    const getURL = `${API_URL}/${productId}`;
+    const getURL = `${environment.API_URL}/${productId}`;
     return this.httpClient.get<Product>(getURL);
   }
 
   addproduct(product: Product): Observable<Product> {
-    return this.httpClient.post<Product>(API_URL, product, httpOptions);
+    return this.httpClient.post<Product>(environment.API_URL, product, httpOptions);
   }
 
   deleteProduct(productId: number): Observable<Product> {
-    const deleteURL = `${API_URL}/${productId}`;
+    const deleteURL = `${environment.API_URL}/${productId}`;
     return this.httpClient.delete<Product>(deleteURL, httpOptions);
   }
 
   updateProduct(productToUpdate: Product): Observable<Product> {
-    return this.httpClient.put<Product>(API_URL, productToUpdate, httpOptions);
+    return this.httpClient.put<Product>(environment.API_URL, productToUpdate, httpOptions);
   }
 
   getCategories(): Observable<Category[]> {
-    return this.httpClient.get<Category[]>(CAT_API_URL);
+    return this.httpClient.get<Category[]>(environment.CAT_API_URL);
   }
 
   getCategory(id: number): Category {
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  API_URL: 'http://localhost:8080/products/api',
+  CAT_API_URL: 'http://localhost:8080/products/api/category',
+};
