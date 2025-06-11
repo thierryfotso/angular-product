10337a8c4e19f0bcc709e91d15f8734f8a6a1387
Thierry FOTSO <fotsonono2003@yahoo.fr>
2025-06-11 15:51:16 +0200

    - use Toastr to display message confirmation email after registering

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideAnimations(),
+    provideToastr(),
   ],
 };
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { User } from '../model/user.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,17 +13,17 @@ import { User } from '../model/user.model';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent implements OnInit {
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private authService: AuthService,
-    private router: Router
-  ) {}
-
   public user = new User();
   confirmPassword?: string;
   myForm!: FormGroup;
   error!: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.myForm = this.formBuilder.group({
@@ -37,7 +38,7 @@ export class RegisterComponent implements OnInit {
     this.authService.register(this.user).subscribe({
       next: (result) => {
         this.authService.setRegistredUser(this.user);
-        alert('veillez confirmer votre email');
+        this.toastr.success('veillez confirmer votre email', 'Confirmer votre email');
         this.router.navigate(['/verifEmail']);
       },
       error: (error: any) => {
